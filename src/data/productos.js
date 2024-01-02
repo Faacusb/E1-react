42f7e165fd2288410c612c1a8528ba972624323f
Facu+ <Faacuusb@hotmail.com>
2024-01-02 20:21:30 -0300

    first commit

---
+++ src/data/productos.js
@@ -0,0 +1,19 @@
+export const categorias = [
+    {
+      id: 1,
+      img: "./img/pablo-soriano-Hnp-cs9QVOc-unsplash.jpg", 
+      titulo: "Ankara",
+      precio: $5000,
+      category: "Masculino"
+    },
+    {
+        id: 2,
+        img: "./img/patricio-n2WT4H30L9k-unsplash.jpg", 
+        titulo: "Purple shine",
+        precio: $5600,
+        category: "Femenino"
+      },
+
+    
+
+]
