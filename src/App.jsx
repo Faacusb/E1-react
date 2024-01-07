656671ae78c8fce639de6ded55da1237aae40d69
Facu+ <Faacuusb@hotmail.com>
2024-01-07 16:24:12 -0300

    cambio import

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Footer, FooterContainer } from "./components/Footer/FooterStyles";
 import Hero from "./components/Hero/Hero";
 import Layout from "./components/Layout/Layout";
 import Navbar from "./components/Navbar/Navbar";
+import  SectionProductos from "./components/Productos";
 import CardsProductos from "./components/Productos/CardsProductos";
-// import  SectionProductos from "./components/Productos/CardsProductosStyle";
 
 
 function App() {
